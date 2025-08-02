37183e9e5d664bb6b3cd8d877a34bb98db270a20
Qu3T31MP0RTA <jjlluscon@gmail.com>
2025-08-02 14:12:55 -0600

    Add files via upload

---
+++ tags.js
@@ -0,0 +1,66 @@
+let tags = [];
+const inputTagContainer = document.querySelector("#input-tag");
+const tagsContainer = document.createElement("div");
+const inputTag = document.createElement("span");
+
+inputTag.ariaRoleDescription = "texbox";
+inputTag.contentEditable = "true";
+inputTag.classList.add("input");
+inputTag.focus();
+
+inputTagContainer.classList.add("input-tag-container");
+tagsContainer.classList.add("tag-container")
+inputTagContainer.appendChild(tagsContainer);
+tagsContainer.appendChild(inputTag)
+
+inputTagContainer.addEventListener("click", (e) => {
+    if (
+        e.target.id == "input-tag" ||
+        e.target.classList.contains("tag-container")) {
+        inputTag.focus();
+    }
+});
+inputTag.addEventListener("keydown", (e) => {
+    if (e.key == "Enter" && inputTag.textContent != "") {
+        e.preventDefault();
+        if (!exitsTag(inputTag.textContent)) {
+            tags.push(inputTag.textContent);
+            inputTag.textContent = "";
+            renderTags();
+        }
+    } else if (e.key == "Backspace" && inputTag.textContent == "" && tags.length > 0) {
+        tags.pop();
+        renderTags();
+    }
+});
+function renderTags() {
+    tagsContainer.innerHTML = "";
+    const html = tags.map((tag) => {
+        const tagElement = document.createElement("div");
+        const tagButton = document.createElement("button");
+        tagElement.classList.add("tag-item");
+        tagButton.textContent = "X";
+        tagButton.addEventListener("click", (e) => {
+            removeTag(tag);
+
+        });
+        tagElement.appendChild(document.createTextNode(tag));
+        tagElement.appendChild(tagButton);
+        return tagElement;
+    });
+
+    html.forEach(Element => {
+        tagsContainer.append(Element);
+    });
+    tagsContainer.appendChild(inputTag);
+    inputTag.focus();
+}
+function exitsTag(value) {
+    return tags.includes(value);
+
+}
+function removeTag(value) {
+    tags = tags.filter(tag => tag != value);
+    renderTags();
+
+}
